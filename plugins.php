121d969ce8c7aeb37fb4e24fc81bac093a1559ac
y3 <imy3mnx@gmail.com>
2017-09-26 23:06:20 +0530

    Test Plugin v1

---
+++ plugins.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: Test
+* Description: Plugin which print hello world on the dashboard screen installed
+* Version: 1.0 
+* Author: yatin_mangoblogger
+*/
+ echo "<h1>Hello World! </h1>";
+
+ ?>
